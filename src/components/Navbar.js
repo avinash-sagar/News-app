056efd4c50fedf1d5021d3f8a28fb4b08573bc4a
Avinash <avinashsagar913@gmail.com>
2022-03-22 16:54:23 +0530

    First commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react'
+import {
+ 
+  Link
+} from "react-router-dom";
+
+
+export default class Navbar extends Component {
+
+    
+  render() {
+   
+    return (
+      <div><nav className="navbar  navbar-expand-lg navbar-dark bg-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/"><img  src="https://1000logos.net/wp-content/uploads/2021/02/Sky-News-Logo-1995.png" className='img' alt="" /></Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <Link className="nav-link bar " aria-current="page" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link bar"  to="/business">Business</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link bar"  to="/entertainment">Entertainment</Link>
+            </li>
+            
+            <li className="nav-item">
+              <Link className="nav-link bar"  to="/health">Health</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link bar"  to="/science">Science</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link bar"  to="/sports">Sports</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link bar"  to="/technology">technology</Link>
+            </li>
+            
+           
+          
+          </ul>
+         
+        </div>
+      </div>
+    </nav></div>
+    )
+  }
+}
